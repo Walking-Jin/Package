4243982d4294e73c8cfffb94e9e146110deedde6
lanou <lanou@wangjin.local>
2016-04-18 16:46:42 +0800

    第一次创建

---
+++ Package/Package/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Package
+//
+//  Created by lanou on 16/4/18.
+//  Copyright © 2016年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+//分而非附件二
+
+
+@end
+
---
+++ Package/Package/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Package
+//
+//  Created by lanou on 16/4/18.
+//  Copyright © 2016年 WJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //efewfwef
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
